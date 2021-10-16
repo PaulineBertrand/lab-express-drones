af8c3e61975dd3692c7a6c40c4ccc6f96ed89fc6
Pauline Bertrand <bertrand.pauline@gmail.com>
2021-10-16 15:26:31 +0200

    iterations1-2-3

--- routes/drones.js
+++ routes/drones.js
@@ -2,21 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const droneModel = require('./../models/Drone.model.js')
+
+// trying the then/catch this time around
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  const allDrones = droneModel.find()
+  .then((allDrones) => {
+    res.render('drones/list.hbs', { allDrones, myCSS: "drone-list.css" })
+  })
+  .catch((err) => console.log("Error when loading all drones: ", err))
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/createForm.hbs', {title: "Create a new drone", myCSS: "createForm.css"})
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  droneModel.create(req.body)
+  .then(() => res.redirect('/drones'))
+  .catch((err) => console.log("Error when creating a new drone: ", err))
 });
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
